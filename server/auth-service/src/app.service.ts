c671b07df3c43b8e9931945ea8471018cb64d47f
Viacheslav Iziumenko <v.iziumenko@mazyway.com>
2020-07-24 19:30:07 +0300

    fix debugger

--- server/auth-service/src/app.service.ts
+++ server/auth-service/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const test = { a: 'b' }
+    console.log('aaaaaaaaaaaaa');
+    
     return 'Hello World!';
   }
 }
